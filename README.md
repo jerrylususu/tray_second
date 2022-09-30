42c03c85181b1e2dd4cb390243967884c3623455
Neko Null <jerrylu.mail@qq.com>
2022-09-30 23:15:20 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Tray Second
+
+![](second.png)
+
+Show current send on system tray. Slight modification based on code from [kas/percentage](https://github.com/kas/percentage).
+
+## Installing
+
+1. [Download the latest release](https://github.com/jerrylususu/tray_second/releases)
+1. Put seconds.exe in your startup folder
+   1. To get to your startup folder, press Windows+R, type "shell:startup", then press enter
+
---
+++ percentage/percentage/TrayIcon.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace percentage
+{
+    class TrayIcon
+    {
+        [DllImport("user32.dll", CharSet=CharSet.Auto)]
+        static extern bool DestroyIcon(IntPtr handle);
+
+        private const int fontSize = 18;
+        private const string font = "Segoe UI";
+
+        private NotifyIcon notifyIcon;
+
+        public TrayIcon()
+        {
+            ContextMenu contextMenu = new ContextMenu();
+            MenuItem menuItem = new MenuItem();
+
+            notifyIcon = new NotifyIcon();
+
+            contextMenu.MenuItems.AddRange(new MenuItem[] { menuItem });
+
+            menuItem.Click += new System.EventHandler(MenuItemClick);
+            menuItem.Index = 0;
+            menuItem.Text = "E&xit";
+
+            notifyIcon.ContextMenu = contextMenu;
+            notifyIcon.Visible = true;
+
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(TimerTick);
+            timer.Start();
+        }
+
+        private Bitmap GetTextBitmap(String text, Font font, Color fontColor)
+        {
+            SizeF imageSize = GetStringImageSize(text, font);
+            Bitmap bitmap = new Bitmap((int)imageSize.Width, (int)imageSize.Height);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.Clear(Color.FromArgb(0, 0, 0, 0));
+                using (Brush brush = new SolidBrush(fontColor))
+                {
+                    graphics.DrawString(text, font, brush, 0, 0);
+                    graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+                    graphics.Save();
+                }
+            }
+            return bitmap;
+        }
+
+        private static SizeF GetStringImageSize(string text, Font font)
+        {
+            using (Image image = new Bitmap(1, 1))
+            using (Graphics graphics = Graphics.FromImage(image))
+                return graphics.MeasureString(text, font);
+        }
+
+        private void MenuItemClick(object sender, EventArgs e)
+        {
+            notifyIcon.Visible = false;
+            notifyIcon.Dispose();
+            Application.Exit();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            // PowerStatus powerStatus = SystemInformation.PowerStatus;
+            // String percentage = (powerStatus.BatteryLifePercent * 100).ToString();
+            //bool isCharging = SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Online;
+            DateTime now = DateTime.Now;
+            String bitmapText = now.ToString("ss");
+            using (Bitmap bitmap = new Bitmap(GetTextBitmap(bitmapText, new Font(font, fontSize), Color.White)))
+            {
+                System.IntPtr intPtr = bitmap.GetHicon();
+                try
+                {
+                    using (Icon icon = Icon.FromHandle(intPtr))
+                    {
+                        notifyIcon.Icon = icon;
+                        String toolTipText = now.ToString("HH:mm:ss");
+                        notifyIcon.Text = toolTipText;
+                    }
+                }
+                finally
+                {
+                    DestroyIcon(intPtr);
+                }
+            }
+        }
+    }
+}
